89f3498dbdc7b313e154354cfd8fcfacc3d6e7d1
Your Name <marielmetalios@gmail.com>
2025-02-27 13:34:07 -0500

    updated seeds

---
+++ server/src/seeds/userSeeds.ts
@@ -0,0 +1,14 @@
+export const usersSeeds = [
+    {
+        'username': 'marielTest',
+        'email': 'mar@gmail.com',
+        'password': 'placeholder',
+        
+    },
+    {
+        'username': 'joshTest',
+        'email': 'josh@gmail.com',
+        'password': 'placeholder2',
+        
+    }
+]
